5b86be0e1ba239ffec1afe777a4ba4fe45ece420
Marta Tao Fernández Navas <martafernandeznavas@gmail.com>
2023-05-19 18:51:43 +0200

    Fixed error on null cart view

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -4,44 +4,50 @@
 @extends('layouts.layout')
 
 @section('content')
-<table id="cart" class="table table-hover table-condensed">
-    <thead>
-        <tr>
-            <th style="width:50%">Product</th>
-            <th style="width:10%">Price</th>
-            <th style="width:8%">Quantity</th>
-        </tr>
-    </thead>
-    <tbody>
+    @if (!is_null(auth()->user()->carrito))
+        <table id="cart" class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <th style="width:50%">Product</th>
+                    <th style="width:10%">Price</th>
+                    <th style="width:8%">Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
                 <tr data-id="">
-                        <td data-th="Product">{{auth()->user()->carrito->nombre}}</td>
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <h4 class="nomargin"></h4>
-                            </div>
+                    <td data-th="Product">{{ auth()->user()->carrito->nombre }}</td>
+                    <div class="row">
+                        <div class="col-sm-9">
+                            <h4 class="nomargin"></h4>
                         </div>
+                    </div>
                     </td>
-                    <td data-th="Price">{{auth()->user()->carrito->total}}</td>
-                    <td data-th="Quantity">{{auth()->user()->carrito->cantidad}}</td>
+                    <td data-th="Price">{{ auth()->user()->carrito->total }}</td>
+                    <td data-th="Quantity">{{ auth()->user()->carrito->cantidad }}</td>
                     <td class="actions" data-th="">
                         <button class="btn btn-danger btn-sm cart_remove"><i class="fa fa-trash-o"></i> Delete</button>
                     </td>
                 </tr>
-    </tbody>
-    <tfoot>
-        <tr>
-            <td colspan="5" class="text-right"><h3><strong></strong></h3></td>
-        </tr>
-        <tr>
-            <td colspan="5" class="text-right">
-                <a href="{{ route('product.index')}}" class="btn btn-danger"> <i class="fa fa-arrow-left"></i> Continue Shopping</a>
-                <button class="btn btn-success"><i class="fa fa-money"></i> Checkout</button>
-            </td>
-        </tr>
-    </tfoot>
-</table>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="text-right">
+                        <h3><strong></strong></h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="text-right">
+                        <a href="{{ route('product.index') }}" class="btn btn-danger"> <i class="fa fa-arrow-left"></i>
+                            Continue Shopping</a>
+                        <button class="btn btn-success"><i class="fa fa-money"></i> Checkout</button>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    @else
+        No hay nada en el carrito aún
+    @endif
 @endsection
 
 @section('scripts')
-
 @endsection
