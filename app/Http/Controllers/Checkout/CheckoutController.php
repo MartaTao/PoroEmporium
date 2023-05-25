230d9cde65853ff1bd045e66e2768e41da90a2d8
Alejandro <alejandrodirm52@gmail.com>
2023-05-25 10:03:43 +0200

    fixed the problem with the layout, it was missing to bring categories

--- app/Http/Controllers/Checkout/CheckoutController.php
+++ app/Http/Controllers/Checkout/CheckoutController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Checkout;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Carrito\Carrito;
+use App\Models\Categorie\Categorie;
 
 class CheckoutController extends Controller
 {
     public function show()
-    {
+    {   
+        $categorias = Categorie::all();
         $cart = session()->get('cart', []);
         $total = 0;
     
@@ -17,6 +19,6 @@ class CheckoutController extends Controller
             $total += $producto['precio'] * $producto['cantidad'];
         }
     
-        return view('checkout.checkout', compact('cart', 'total'));
+        return view('checkout.checkout', compact('categorias','cart', 'total'));
     }
 }    
