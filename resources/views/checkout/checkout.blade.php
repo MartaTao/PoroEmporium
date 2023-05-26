4af9e02c27455a4b3616ebab09ef3d425e03af8c
Alejandro <alejandrodirm52@gmail.com>
2023-05-26 10:19:28 +0200

    change style checkout

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -1,55 +1,63 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex items-center mt-2.5 mb-5 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 _wYiJGbRZyFZeCc8y7Sf _Ybd3WwuTVljUT4vEaM3 mveJTCIb2WII7J4sY22F mngKhi_Rv06PF57lblDI _YxZw_O8dWkTljptcO7z SWDELhWFjL8JxEtm91_o _1jTZ8KXRZul60S6czNi">
-    <h3 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white hD0sTTDgbxakubcHVW2X vyo_A8gnQD1QWDPglr3h IOPhczRgtphv6NdNBDjj OyABRrnTV_kvHV7dJ0uE">Card Details</h3>
-    <form action="{{ route('checkout.pay') }}" method="POST">
-        <div class="xCPtuxM4_gihvpPwv9bX Nu4HUn5EQpnNJ1itNkrd Bcw8VuwRWYxPGjWjS6Ig EyjJPKD7jgGRBhaLpRVI AqVNvLG_H6VHhym2yKMp">
-            <div>
-                <label for="full_name" class="_Vb9igHms0hI1PQcvp_S TR_P65x9ie7j6uxFo_Cs c8dCx6gnV43hTOLV6ks5 ezMFUVl744lvw6ht0lFe __9sbu0yrzdhGIkLWNXl OyABRrnTV_kvHV7dJ0uE">Name *</label>
-                <input type="text" name="full_name" id="full_name" class="fzhbbDQ686T8arwvi_bJ jtAJHOc7mn7b4IKRO59D pXhVRBC8yaUNllmIWxln vpDN1VEJLu5FmLkr5WCk __9sbu0yrzdhGIkLWNXl gx_pYWtAG2cJIqhquLbx mveJTCIb2WII7J4sY22F GdTcGtoKP5_bET3syLDl LceKfSImrGKQrtDGkpBV _Vb9igHms0hI1PQcvp_S t6gkcSf0Bt4MLItXvDJ_ olxDi3yL6f0gpdsOFDhx jqg6J89cvxmDiFpnV56r Mmx5lX7HVdrWCgh3EpTP H7KQDhgKsqZaTUouEUQL OyABRrnTV_kvHV7dJ0uE KpCMWe32PQyrSFbZVput q6szSHqGtBufkToFe_s5" placeholder="Full name on card" required="">
-            </div>
-            
-            <div>
-                <label for="card_number" class="_Vb9igHms0hI1PQcvp_S TR_P65x9ie7j6uxFo_Cs c8dCx6gnV43hTOLV6ks5 ezMFUVl744lvw6ht0lFe __9sbu0yrzdhGIkLWNXl OyABRrnTV_kvHV7dJ0uE">Card Number *</label>
-                <input class="" autocomplete="cc-number" autocorrect="off" spellcheck="false" id="card_number" name="cardNumber" type="text" inputmode="numeric" aria-label="Card number" placeholder="1234 1234 1234 1234" aria-invalid="true" value="">
-            </div>
-            <div class="form-group">
-                <label for="expiration_date" class="_Vb9igHms0hI1PQcvp_S TR_P65x9ie7j6uxFo_Cs c8dCx6gnV43hTOLV6ks5 ezMFUVl744lvw6ht0lFe __9sbu0yrzdhGIkLWNXl OyABRrnTV_kvHV7dJ0uE">Expiration Date *</label>
-                <input type="date" class="form-control fzhbbDQ686T8arwvi_bJ jtAJHOc7mn7b4IKRO59D pXhVRBC8yaUNllmIWxln vpDN1VEJLu5FmLkr5WCk __9sbu0yrzdhGIkLWNXl gx_pYWtAG2cJIqhquLbx mveJTCIb2WII7J4sY22F GdTcGtoKP5_bET3syLDl LceKfSImrGKQrtDGkpBV _Vb9igHms0hI1PQcvp_S t6gkcSf0Bt4MLItXvDJ_ olxDi3yL6f0gpdsOFDhx jqg6J89cvxmDiFpnV56r Mmx5lX7HVdrWCgh3EpTP H7KQDhgKsqZaTUouEUQL OyABRrnTV_kvHV7dJ0uE KpCMWe32PQyrSFbZVput q6szSHqGtBufkToFe_s5" id="expiration_date" name="expiration_date" required>
-            </div>
+<div class="grid grid-cols-2 gap-4">
+    <div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table id="cart" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class=" text-xs text-gray-700 uppercase dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Product</th>
+                        <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Price</th>
+                        <th scope="col" class="px-6 py-3">Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @for ($i=0;$i< count($cart);$i++) <tr data-id="" class="border-b border-gray-200 dark:border-gray-700">
+                        <td data-th="Product" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{$cart[$i]['nombre']}}</td>
+                        <td data-th="Price" class="px-6 py-4 bg-gray-50 dark:bg-gray-800">{{$cart[$i]['precio']}}</td>
+                        <td data-th="Quantity" class="px-6 py-3">{{$cart[$i]['cantidad']}}</td>
+                        <td class="actions" data-th="">
+                        </td>
+                        </tr>
+                        @endfor
+                        <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Total</th>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800" data-th="total">${{ $total }}</td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div>
+        <div class="flex justify-center">
+            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <h3 class="text-3xl font-bold text-gray-900 dark:text-white">Card Details</h3>
+                <form action="{{ route('checkout.pay') }}" method="POST" class="px-8 py-6">
+                    <div class="w-full max-w-md bg-white shadow-md rounded-lg px-8 py-6 xCPtuxM4_gihvpPwv9bX Nu4HUn5EQpnNJ1itNkrd Bcw8VuwRWYxPGjWjS6Ig EyjJPKD7jgGRBhaLpRVI AqVNvLG_H6VHhym2yKMp">
+                        <div>
+                            <label for="card_number" class="block text-sm font-medium text-gray-700 dark:text-white">Card Number *</label>
+                            <input type="text" name="card_number" id="card_number" class="mt-1 block w-full py-2 px-3 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="1234 1234 1234 1234" autocomplete="cc-number" autocorrect="off" spellcheck="false" inputmode="numeric" aria-label="Card number" aria-invalid="true">
+                        </div>
+                        <div>
+                            <label for="expiration_date" class="block text-sm font-medium text-gray-700 dark:text-white">Expiration Date *</label>
+                            <input type="date" name="expiration_date" id="expiration_date" class="mt-1 block w-full py-2 px-3 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        </div>
+                        <div>
+                            <label for="full_name" class="block text-sm font-medium text-gray-700 dark:text-white">Name *</label>
+                            <input type="text" name="full_name" id="full_name" class="mt-1 block w-full py-2 px-3 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="Full name on card">
+                        </div>
 
-            <div>
-                <label for="cvv" class="_Vb9igHms0hI1PQcvp_S TR_P65x9ie7j6uxFo_Cs c8dCx6gnV43hTOLV6ks5 ezMFUVl744lvw6ht0lFe __9sbu0yrzdhGIkLWNXl OyABRrnTV_kvHV7dJ0uE">CVC *</label>
-                <input type="number" name="cvv" id="cvv" class="fzhbbDQ686T8arwvi_bJ jtAJHOc7mn7b4IKRO59D pXhVRBC8yaUNllmIWxln vpDN1VEJLu5FmLkr5WCk __9sbu0yrzdhGIkLWNXl gx_pYWtAG2cJIqhquLbx mveJTCIb2WII7J4sY22F GdTcGtoKP5_bET3syLDl LceKfSImrGKQrtDGkpBV _Vb9igHms0hI1PQcvp_S t6gkcSf0Bt4MLItXvDJ_ olxDi3yL6f0gpdsOFDhx jqg6J89cvxmDiFpnV56r Mmx5lX7HVdrWCgh3EpTP H7KQDhgKsqZaTUouEUQL OyABRrnTV_kvHV7dJ0uE KpCMWe32PQyrSFbZVput q6szSHqGtBufkToFe_s5" placeholder="•••" required="">
+                        <div>
+                            <label for="cvv" class="block text-sm font-medium text-gray-700 dark:text-white">CVC *</label>
+                            <input type="number" name="cvv" id="cvv" class="mt-1 block w-full py-2 px-3 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 rounded-md shadow-sm text-gray-900 dark:text-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" placeholder="•••" required="">
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-danger btn-sm cart_remove px-6 py-3">
+                                Pay Now
+                            </button>
+                        </div>
+                </form>
             </div>
-            <div>
-                <button type="submit" class="btn btn-danger btn-sm cart_remove px-6 py-3">
-                    Pay Now
-                </button>
-            </div>
-    </form>
+        </div>
+    </div>
 </div>
-</div>
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table id="cart" class="w-full text-sm text-left text-gray-500 dark:text-gray-400>
-<thead class=" text-xs text-gray-700 uppercase dark:text-gray-400">
-        <tr>
-            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Product</th>
-            <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Price</th>
-            <th scope="col" class="px-6 py-3">Quantity</th>
-        </tr>
-        </thead>
-        <tbody>
-            @for ($i=0;$i< count($cart);$i++) <tr data-id="" class="border-b border-gray-200 dark:border-gray-700">
-                <td data-th="Product" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">{{$cart[$i]['nombre']}}</td>
-                <td data-th="Price" class="px-6 py-4 bg-gray-50 dark:bg-gray-800">{{$cart[$i]['precio']}}</td>
-                <td data-th="Quantity" class="px-6 py-3">{{$cart[$i]['cantidad']}}</td>
-                <td class="actions" data-th="">
-                </td>
-                </tr>
-                @endfor
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">Total</th>
-                <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800" data-th="total">${{ $total }}</td>
-        </tbody>
-        @endsection
+@endsection
