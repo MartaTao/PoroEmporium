3a0efe02544f83f72a3231a6b3dfb315f6c30979
Alejandro <alejandrodirm52@gmail.com>
2023-05-31 10:10:03 +0200

    migrate

--- database/migrations/2023_05_31_075732_create_orders_table.php
+++ database/migrations/2023_05_31_075732_create_orders_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->dateTime('date');
+            $table->decimal('price', 8, 2);
+            $table->string('status');
             $table->timestamps();
+            
         });
     }
 
