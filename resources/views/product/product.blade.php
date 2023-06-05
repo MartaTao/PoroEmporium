e8cbe871c6e8305a86fa594292b7f80dbced1495
Alejandro <alejandrodirm52@gmail.com>
2023-06-05 10:05:25 +0200

    if there are no products left the button  is deactivated for that product

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -125,6 +125,7 @@
                             <div class="flex items-center justify-center">
                                 <button href="{{ route('cart.addToCart', $product->id) }}"
                                     onclick="this.parentNode.submit();"
+                                    {{ $product->cantidad > 0 ? '' : 'disabled' }}
                                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                     <svg aria-hidden="true" class="w-5 h-5 mr-2 -ml-1" fill="currentColor"
                                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
