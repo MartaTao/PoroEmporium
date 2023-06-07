5f304577bc71b07693eee526f96219c084d5a8cf
Alejandro <alejandrodirm52@gmail.com>
2023-06-07 18:00:35 +0200

    change style order

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -4,16 +4,16 @@
 <div class="container mx-auto py-10 flex justify-center items-center">
     <div class="flowbite-card flowbite-card-bordered flowbite-shadow-xl">
         <div class="flowbite-card-content">
-            <h1 class="flowbite-heading flowbite-h2">Mis órdenes</h1>
+            <h1 class="items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Mis órdenes</h1>
             <hr class="flowbite-hr my-4">
             @if ($orders->isEmpty())
                 <p>No se encontraron órdenes.</p>
             @else
                 <ul>
                     @foreach ($orders as $order)
-                        <dl class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                            <div class="flowbite-card-content">
-                                <dt class="mb-2  font-bold tracking-tight text-gray-900 dark:text-white">Número de orden:</dt>
+                        <dl class=" items-center text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <div class="flowbite-card-content mt-2">
+                                <dt class="flowbite-text-gray-600 flowbite-text-lg">Número de orden:</dt>
                                 <dd class="flowbite-text-lg flowbite-font-semibold">{{ $order->id }}</dd>
                             </div>
                             <div class="flowbite-card-content mt-2">
