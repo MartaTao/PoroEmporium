b41026f260312f9664cbf3d1a201cbeebac8fc2a
Marta Tao Fernández Navas <martafernandeznavas@gmail.com>
2023-06-07 17:46:47 +0200

    Fixed a typo with sejected email

--- resources/views/emails/rejected-restock.blade.php
+++ resources/views/emails/rejected-restock.blade.php
@@ -4,7 +4,7 @@
     <div class="bg-white rounded-md shadow-md p-8">
     <p>Estimado PoroEmporium,</p>
 
-    <p>Agradecemos que se haya puesto en contacto con nosotros para seguir vendiendo nuestro producto. Su solicitud ha sido rechaza. Lamentablemente no quedan existencias del producto o es parte de una edición limitada</p>
+    <p>Agradecemos que se haya puesto en contacto con nosotros para seguir vendiendo nuestro producto. Su solicitud ha sido rechazada. Lamentablemente no quedan existencias del producto o es parte de una edición limitada</p>
 
     <p>Gracias por su atención.</p>
     <p>Saludos,</p>
