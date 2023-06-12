6d2bc5a88022b8b36fdd0c0575acc18cff5349d7
Alejandro <alejandrodirm52@gmail.com>
2023-06-12 11:42:28 +0200

    view value mastercard and visa

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -49,8 +49,8 @@
                     <div class="relative z-0 w-full mb-6 group">
                         <label for="cardtype" class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Card Type *</label>
                         <select name="cardtype" id="cardtype" class="block py-2.5 px-0 w-full text-sm text-gray-900 dark:text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer">
-                            <option value="visa">Visa</option>
-                            <option value="mastercard">Mastercard</option>
+                            <option value="visa" style="color: gray;">Visa</option>
+                            <option value="mastercard" style="color: gray;">Mastercard</option>
                         </select>
                     </div>
                     <div class="relative z-0 w-full mb-6 group">
