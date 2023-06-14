573f03d0d8f353e7230773b0a3f55672e41e50e7
Marta Tao Fernández Navas <martafernandeznavas@gmail.com>
2023-06-14 22:35:59 +0200

    Fix change pass and email layout

--- resources/views/config/change_password.blade.php
+++ resources/views/config/change_password.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class=" py-4 sm:pt-0">
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="flex flex-col  px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div class="w-full rounded-lg md:mt-0 sm:max-w-md xl:p-0">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl">
